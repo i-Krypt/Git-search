cab1f5a85d56dbb8b9921409054709fd97543e7e
i-Krypt <vamay505@gmail.com>
2019-08-26 08:18:38 +0300

    exported class gitsearch form component

--- src/app/git-form/git-form.component.ts
+++ src/app/git-form/git-form.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
 @Component({
-  selector: 'app-git-form',
-  templateUrl: './git-form.component.html',
-  styleUrls: ['./git-form.component.css']
+ selector: 'app-gitsearch-form',
+ templateUrl: './gitsearch-form.component.html',
+ styleUrls: ['./gitsearch-form.component.css']
 })
-export class GitFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class GitsearchFormComponent implements OnInit {
+ @Output() emitSearch = new EventEmitter<any>()
+ searchTerm:string;
+ search(){
+   this.emitSearch.emit(this.searchTerm);
+ }
+ constructor() { }
+ ngOnInit() {
+ }
 }
