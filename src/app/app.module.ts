475cd51d8c2196649b483aedba809612d718d8fc
i-Krypt <vamay505@gmail.com>
2019-08-26 07:49:33 +0300

    added a ui component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UiComponent } from './ui/ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UiComponent
   ],
   imports: [
     BrowserModule,
