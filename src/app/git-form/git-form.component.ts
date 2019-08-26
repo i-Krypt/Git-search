9bcd8c1d84f835104292a64529a9a8a471576d88
i-Krypt <vamay505@gmail.com>
2019-08-26 08:28:04 +0300

    imported an event emitter to the repo

--- src/app/git-form/git-form.component.ts
+++ src/app/git-form/git-form.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
 @Component({
- selector: 'app-gitsearch-form',
- templateUrl: './gitsearch-form.component.html',
- styleUrls: ['./gitsearch-form.component.css']
+  selector: 'app-git-form',
+  templateUrl: './git-form.component.html',
+  styleUrls: ['./git-form.component.css']
 })
-export class GitsearchFormComponent implements OnInit {
- @Output() emitSearch = new EventEmitter<any>()
- searchTerm:string;
+export class GitFormComponent implements OnInit {
+  @Output() emitSearch = new EventEmitter<any>()
+
+  searchTerm:string;
  search(){
    this.emitSearch.emit(this.searchTerm);
  }
- constructor() { }
- ngOnInit() {
- }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
