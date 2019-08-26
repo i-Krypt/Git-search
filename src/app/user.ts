15a864be17b29ba12aa6360d561d4d524619631b
i-Krypt <vamay505@gmail.com>
2019-08-26 07:59:28 +0300

    added a constructot to user class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,4 @@
 export class User {
+
+    constructor( public user:string,public avatar_url:any, public login: string, public public_repos:number, public followers:number,public following:number,public url:any ){   }
 }
